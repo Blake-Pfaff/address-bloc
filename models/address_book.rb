47f986ca21754beae16ec64f0b8f870f14a46159
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-09-30 15:32:18 -0500

    adds ability to search for name using binary al

--- models/address_book.rb
+++ models/address_book.rb
@@ -40,5 +40,30 @@
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end     # Implementation goes here
    end
+   # Search AddressBook for a specific entry by name
+   def binary_search(name)
+
+     #
+     lower = 0
+     upper = entries.length - 1
+
+     #iterate thtough the loop while lower is less then or eq to upper index
+     while lower <= upper
+       #takes sum of lower & upper divides in half
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       #compares name searching for to name of middle of index.
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     #if didide until no match is found, rtn nil
+     return nil
+   end
 end
 
