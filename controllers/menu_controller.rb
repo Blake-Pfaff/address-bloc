d2f2810fbe9a8980b0ac9d416a6270ff8b2ee0ec
Blake Pfaff <blake.a.pfaff2@gmail.com>
2017-10-01 10:17:34 -0500

    addes last featurs to menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -102,11 +102,46 @@
    end
 
    def search_entries
+     #gets name user wants to search for and stores it in name
+     print "Search by name: "
+     name = gets.chomp
+     #call search on address_book rtn nil or never rtn an empty strng
+     match = address_book.binary_search(name)
+     system "clear"
+     #check if search rtn match.  if true puts match.to_s if false pust message
+     if match
+       puts match.to_s
+       search_submenu(match)
+     else
+       puts "No match found for #{name}"
+     end
+
    end
 
    def read_csv
+    #prompt user for name of CSV file to import
+     print "Enter CSV file to import: "
+      file_name = gets.chomp
+
+      # check to see if file name is empty.  if so rtn user back t othe main menu
+     if file_name.empty?
+       system "clear"
+       puts "No CSV file read"
+       main_menu
+     end
+
+     # import file with import_from_csa
+     begin #prtcts the prog from crashing also uses "rescue"
+       entry_count = address_book.import_from_csv(file_name).count
+       system "clear"
+       puts "#{entry_count} new entries added from #{file_name}"
+     rescue
+       puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+       read_csv
+     end
    end
-      def entry_submenu(entry)
+
+   def entry_submenu(entry)
      #displays submenu options
      puts "n - next entry"
      puts "d - delete entry"
@@ -121,7 +156,12 @@
        when "n"
      #deleting & editing later
        when "d"
+        #user views submenu & press d, delete_entry called, ctrl will rtn to view_all_entries & display nxt entrie
+        delete_entry(entry)
        when "e"
+        # call edit_entry when user presse e.  display syb-menu with entry_submenu under edit
+        edit_entry(entry)
+        entry_submenu(entry)
      # returns the user to the main menu
        when "m"
          system "clear"
@@ -132,4 +172,56 @@
          entry_submenu(entry)
      end
     end
+
+   def delete_entry(entry)
+     address_book.entries.delete(entry)
+     puts "#{entry.name} has been deleted"
+   end
+
+   def edit_entry(entry)
+     #preform a series of print stmnts w/ chomp.  each get.chomp stmnt gathers user input & assigns it to vars
+     print "Updated name: "
+     name = gets.chomp
+     print "Updated phone number: "
+     phone_number = gets.chomp
+     print "Updated email: "
+     email = gets.chomp
+     # "!" to set attrs on entry if vaild attr was read from user input
+     entry.name = name if !name.empty?
+     entry.phone_number = phone_number if !phone_number.empty?
+     entry.email = email if !email.empty?
+     system "clear"
+     # prints entry with upadted attrs
+     puts "Updated entry:"
+     puts entry
+   end
+
+   def search_submenu(entry)
+     #print submenu for entry
+     puts "\nd - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+     # save the user input to selection
+     selection = gets.chomp
+
+     #case statement & specific action from user input.
+     case selection
+       when "d"
+         system "clear"
+         delete_entry(entry)
+         main_menu
+       when "e"
+         edit_entry(entry)
+         system "clear"
+         main_menu
+       when "m"
+         system "clear"
+         main_menu
+       else
+         system "clear"
+         puts "#{selection} is not a valid input"
+         puts entry.to_s
+         search_submenu(entry)
+     end
+   end
  end
